b7b0edc66e04883dea456f359dea7012490d8c1f
2015nitin.aswal@ves.ac.in <ni3aswal@gmail.com>
2020-05-10 11:49:38 +0530

    first commit

--- social/settings.py
+++ social/settings.py
@@ -14,7 +14,6 @@ import os
 import django_heroku
 
 # Activate Django-Heroku.
-django_heroku.settings(locals())
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -138,3 +137,4 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
+django_heroku.settings(locals())
