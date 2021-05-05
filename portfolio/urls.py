3eb1f936924cda99c3ff9f8286ce0a3366000889
narottamaswal <65083220+narottamaswal@users.noreply.github.com>
2021-05-05 20:42:46 +0530

    Update urls.py

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import index,download_file
 
 urlpatterns=[
-    path('',index,name="index"),    path('download/', download_file),
+    path('',index,name="index"),    path('/download', download_file),
 
 ]
