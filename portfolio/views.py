21c4e66c7ea1b9149257e52dce2232e8f727bef6
Narottam Aswal <narottam98678@gmail.com>
2021-05-05 19:13:57 +0530

    first

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,6 +1,32 @@
 from django.shortcuts import render
+# Import mimetypes module
+import mimetypes
+# import os module
+import os
+# Import HttpResponse module
+from django.http.response import HttpResponse
+
 
 # Create your views here.
 
 def index(request):
     return render(request,"portfolio/index.html")
+
+
+def download_file(request):
+    # Define Django project base directory
+    BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    # Define text file name
+    filename = 'CV Narottam Aswal.pdf'
+    # Define the full file path
+    filepath = BASE_DIR + '/static' + filename
+    # Open the file for reading content
+    path = open(filepath, 'r')
+    # Set the mime type
+    mime_type, _ = mimetypes.guess_type(filepath)
+    # Set the return value of the HttpResponse
+    response = HttpResponse(path, content_type=mime_type)
+    # Set the HTTP header for sending to browser
+    response['Content-Disposition'] = "attachment; filename=%s" % filename
+    # Return the response value
+    return response
